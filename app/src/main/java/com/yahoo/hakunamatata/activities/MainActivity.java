4d2461e474693b822dba133c28d62fa46f455683
jonaswu <jonaswu@yahoo-inc.com>
2015-08-29 21:15:24 +0800

    integrate toolbar

--- app/src/main/java/com/yahoo/hakunamatata/activities/MainActivity.java
+++ app/src/main/java/com/yahoo/hakunamatata/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -14,6 +15,12 @@ import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.yahoo.hakunamatata.R;
 import com.yahoo.hakunamatata.adapters.ListingPagerAdapter;
 import com.yahoo.hakunamatata.fragments.ListingFragment;
+import com.yahoo.hakunamatata.network.FacebookClient;
+import com.yahoo.hakunamatata.network.MyJsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity implements ListingFragment.OnFragmentInteractionListener {
@@ -27,6 +34,22 @@ public class MainActivity extends ActionBarActivity implements ListingFragment.O
         setContentView(R.layout.activity_main);
         setUpActionBar();
         setUpPager();
+
+
+        FacebookClient client = RestApplication.getRestClient();
+        client.getPostsOfGroup("173830556282081",
+                new MyJsonHttpResponseHandler(this) {
+                    @Override
+                    public void successCallBack(int statusCode, Header[] headers, Object data) {
+                        JSONObject dataJSON = (JSONObject) data;
+                        Log.e("data", dataJSON.toString());
+                    }
+
+                    @Override
+                    public void errorCallBack() {
+
+                    }
+                });
     }
 
     private void setUpActionBar() {
@@ -34,7 +57,7 @@ public class MainActivity extends ActionBarActivity implements ListingFragment.O
         setSupportActionBar(toolbar);
         toolbar.inflateMenu(R.menu.menu_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_hakuna);
+        // getSupportActionBar().setIcon(R.drawable.ic_hakuna);
 //        if (getSupportActionBar().isShowing() == true) {
 //            getSupportActionBar().setElevation(0);
 //            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.orange3)));
