3759872fc8434c8d9daa42f90c5bb8499cd97b2e
jonaswu <jonaswu@yahoo-inc.com>
2015-09-02 21:30:06 +0800

    stage commit

--- app/src/main/java/com/yahoo/hakunamatata/activities/GuideActivity.java
+++ app/src/main/java/com/yahoo/hakunamatata/activities/GuideActivity.java
@@ -1,8 +1,8 @@
 package com.yahoo.hakunamatata.activities;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
