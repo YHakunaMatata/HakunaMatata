188a051d0281447f7a58f3ca19a0af59fbd5f424
Sinze <sinze@yahoo-inc.com>
2015-09-02 17:33:41 +0800

    Add reply

--- app/src/main/java/com/yahoo/hakunamatata/fragments/ReplyFragment.java
+++ app/src/main/java/com/yahoo/hakunamatata/fragments/ReplyFragment.java
@@ -5,19 +5,26 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import com.yahoo.hakunamatata.R;
 import com.yahoo.hakunamatata.activities.RestApplication;
+import com.yahoo.hakunamatata.interfaces.Progressable;
 import com.yahoo.hakunamatata.models.Post;
 import com.yahoo.hakunamatata.network.FacebookClient;
+import com.yahoo.hakunamatata.network.MyJsonHttpResponseHandler;
+
+import org.apache.http.Header;
 
 public class ReplyFragment extends DialogFragment {
     private Post post;
+    private Progressable progressable;
 
     public static ReplyFragment newInstance(Post post) {
         ReplyFragment fragment = new ReplyFragment();
@@ -37,8 +44,50 @@ public class ReplyFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // set up progress bar
+        progressable = (Progressable) getActivity();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reply, container, false);
+        View view =  inflater.inflate(R.layout.fragment_reply, container, false);
+        final FacebookClient client = RestApplication.getRestClient();
+        final EditText message = (EditText) view.findViewById(R.id.message);
+        // OK
+        view.findViewById(R.id.positive_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressable.setBusy();
+                client.postComment(
+                    post.id,
+                    message.getText().toString(),
+                    new MyJsonHttpResponseHandler(getActivity()) {
+                        @Override
+                        public void successCallBack(int statusCode, Header[] headers, Object data) {
+                            Log.e("my", "post comment success!");
+                            progressable.setFinish();
+                        }
+
+                        @Override
+                        public void errorCallBack() {
+                            Log.e("my", "post comment fail!");
+                            progressable.setFinish();
+                        }
+                    }
+                );
+                dismiss();
+            }
+        });
+
+        // Close
+
+        view.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -50,23 +99,6 @@ public class ReplyFragment extends DialogFragment {
         dialog.setContentView(R.layout.fragment_reply);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        final FacebookClient client = RestApplication.getRestClient();
-        // OK
-        dialog.findViewById(R.id.positive_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
-
-        // Close
-
-        dialog.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
         return dialog;
     }
 }
